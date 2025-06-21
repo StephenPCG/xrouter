a67325e1908d703c58b89ec466322fdcdaebc586
stephen <stephen@gwtest.home.init2.me>
2025-06-21 19:47:43 +0800

    add reload commands

--- xrouter/cli/reload.py
+++ xrouter/cli/reload.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
+import sh
 import typer
-from rich import print
 
 app = typer.Typer(
     no_args_is_help=True,
@@ -10,11 +10,46 @@ app = typer.Typer(
 
 
 @app.command("ifaces")
-def reload_ifaces(
-    ifaces: Annotated[list[str] | None, typer.Argument()] = None,
-):
-    if ifaces:
-        print("reload ifaces:", ifaces)
+def reload_ifaces():
+    from xrouter.gwlib import gw
 
-    else:
-        print("reload all ifaces")
+    gw.run_command(sh.networkctl.bake("reload"))
+
+
+@app.command("route")
+def reload_route():
+    from xrouter.gwlib import gw
+
+    gw.run_command(sh.Command(gw.bin_root / "setup-route.sh"))
+
+
+@app.command("firewall")
+def reload_firewall():
+    from xrouter.gwlib import gw
+
+    gw.run_command(sh.Command(gw.bin_root / "setup-firewall.nft"))
+
+
+@app.command("network")
+def reload_network():
+    reload_ifaces()
+    reload_route()
+    reload_firewall()
+
+
+@app.command("containers")
+def reload_containers(names: Annotated[list[str] | None, typer.Argument()] = None):
+    from xrouter.gwlib import gw
+
+    if not names:
+        names = gw.config.containers.container_names
+
+    for name in names:
+        gw.run_command(sh.systemctl.bake("restart", f"container-{name}.service"))
+
+
+@app.command("dnsmasq")
+def reload_dnsmasq():
+    from xrouter.gwlib import gw
+
+    gw.run_command(sh.systemctl.bake("restart", "dnsmasq"))
